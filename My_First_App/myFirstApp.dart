4343dc79e2de4606aff5db93b0521c00077d5d30
SeshanSaravanan <88974719+SeshanSaravanan@users.noreply.github.com>
2022-04-30 14:30:13 +0530

    Create myFirstApp.dart

---
+++ My_First_App/myFirstApp.dart
@@ -0,0 +1,21 @@
+/* This is my first app with the use of widgets */
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is my default text'),
+      ),
+    );
+  }
+}
